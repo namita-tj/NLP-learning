3cfee5eb1d9176ada384f48572f35fed2cac3205
namita <namita.tj@gmail.com>
2025-04-03 06:56:08 +0200

    Updated with User Input

--- 02-keyword-extraction-tool.py
+++ 02-keyword-extraction-tool.py
@@ -1,18 +1,19 @@
 import spacy
-from PyPDF2 import PDFReader
-from wordcloud import WordCloud
+from PyPDF2 import PdfReader
 import argparse
 
-def keyword_extractor(article):
-  nlp=spacy.load("en-core-web-md")
-  reader=PDFReader(article)
-  text=" ".join([page.extract_text() for page in reader.pages])
+def keyword_extractor():
+  article=input("Enter path to the article : ").strip()
+  nlp=spacy.load("en_core_web_md")
+  try:
+    reader=PdfReader(article)
+    text=" ".join([page.extract_text() for page in reader.pages])
+  except FileNotFoundError:
+    print(f"File not found: {article}")
+    return
   doc=nlp(text)
   keywords=[token.text for token in doc if token.pos_ in ["NOUN","PROPN"]]
   print("Keywords (without duplicates):",set(keywords))
 
 if __name__=="__main__":
-  parser=argparse.ArgumentParser("Extract keywords from a PDF:")
-  parser.add_argument("--pdf",type=str,required=True)
-  args=parser.parse_args()
-  keyword_extractor(args.pdf)
+  keyword_extractor()
